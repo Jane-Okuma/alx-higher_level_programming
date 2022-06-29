66b84bbe3196fc870d0e39271955e3eead12c61c
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-06-30 00:44:00 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 98):
     if i % 2 == 0:
-        code =int(i)
+        code = int(i)
     else:
         code = int(i) + 32
-        print("{}".format(chr(i)), end="")
+        print("{:s}".format(chr(i)), end="")
