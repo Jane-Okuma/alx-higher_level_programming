5c3140c63fd6e0617d7e64a9d198e3b07432b867
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-06-29 15:49:18 -0700

    Comm10

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 def print_last_digit(number):
     if number < 0:
         number = -1 * number
-        return number % 10
+        print(number % 10)
     else:
-        return number % 10
+        print(number % 10)
