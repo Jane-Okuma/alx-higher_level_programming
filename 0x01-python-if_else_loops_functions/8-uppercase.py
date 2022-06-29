4fb023f6132fbe5d69e327a5d477da17ef2242bc
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-06-29 15:33:39 -0700

    Comm9;

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
+    count = 0
     for i in str:
+        count = count + 1
         if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(int(ord(i)) - 32)), end="")
+            code = int(ord(i)) - 32
         else:
-            print("{}".format(i), end="")
-    return "\n"
+            code = int(ord(i))
+        print("{}".format(chr(code)), end="")
+        if count == len(str):
+            print("\n")
