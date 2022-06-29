be8621370c7abf78c9b90c46c8539596d5d7ce72
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-06-29 16:20:17 -0700

    Comm12

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b > 1:
+        mul = a
+        for i in range(1, b):
+            mul = mul * a
+        return mul
+    elif b < 1:
+        mul = a
+        for i in range(1, b):
+            mul = mul * a
+        return 1 / mul
