0d70ee6b84ba00a91ab536213d6a529fe5b89b39
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-07-01 23:12:44 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,23 +5,19 @@ if __name__ == "__main__":
     length = len(argv) - 1
     if length != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print("{:d}".format(1))
-    elif argv[2] == "+" or argv[2] == "-" or argv[2] == "*" or argv[2] == "/":
+        sys.exit(1)
+    elif argv[2] != "+" or argv[2] != "-" or argv[2] != "*" or argv[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
-        print("{:d}".format(1))
+        sys.exit(1)
     else:
         a = int(argv[1])
         b = int(argv[3])
         c = argv[2]
         if c == "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-            print("{:d}".format(0))
         elif c == "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-            print("{:d}".format(0))
         elif c == "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-            print("{:d}".format(0))
         elif c == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-            print("{:d}".format(0))
