acba480b07768533e15f2a57ce2875d128eb2367
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-07-03 21:18:07 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,18 +6,18 @@ if __name__ == "__main__":
     if length != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
+    if argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-        c = argv[2]
-        if c == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif c == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif c == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif c == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        
+     a = int(argv[1])
+     b = int(argv[3])
+     c = argv[2]
+     if c == "+":
+         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+     elif c == "-":
+          print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+     elif c == "*":
+          print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+     elif c == "/":
+          print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
