b52611a399c8d99816e62151edb18af871d961fe
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-07-03 23:50:59 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
     string_list = []
+     new_string = ""
     for i in range(0, len(my_string)):
         string_list.append(my_string[i])
     for i in range(0, len(string_list)):
         if string_list[i] == "C" or string_list[i] == "c":
             continue
         else:
-            print("{}".format(string_list[i]), end="")
+                    new_string = new_string + string_list[i]
+    return(new_string)
