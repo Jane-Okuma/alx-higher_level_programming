5ba033907138c1a2f340f7331b4fa17b4ec0f7ed
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-07-03 21:13:28 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if length != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif argv[2] != "+" and argv[2] != "-" and argv[2] != "*" and argv[2] != "/":
+    elif argv[2] != "+" or argv[2] != "-" or argv[2] != "*" or argv[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     else:
