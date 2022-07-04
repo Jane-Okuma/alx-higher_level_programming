12da18527278181ad9717eb66a8d8fc86c52fe3e
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-07-04 21:52:51 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 2:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+    if len(tuple_b) >= 2:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
     if len(tuple_a) == 1:
-        tuple_a[1] = 0
-    if len(tuple_a) == 0:
-        tuple_a[0] = tuple_b[1] = 0
+        a1 = tuple_a[0]
+        a2 = 0
     if len(tuple_b) == 1:
-        tuple_b[1] = 0
+        b1 = tuple_b[0]
+        b2 = 0
+    if len(tuple_a) == 0:
+        a1 = 0
+        a2 = 0
     if len(tuple_b) == 0:
-        tuple_b[0] = tuple_b[1] = 0
+        b1 = 0
+        b2 = 0
 
-    sum_0 = tuple_a[0] + tuple_b[0]
-    sum_1 = tuple_a[1] + tuple_b[1]
+    sum_0 = a1 + b1
+    sum_1 = a2 + b2
     ls = []
     ls.append(sum_0)
     ls.append(sum_1)
