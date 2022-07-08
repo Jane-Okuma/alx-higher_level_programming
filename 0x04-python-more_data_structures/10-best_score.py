ec1d0c517945fe66104e28e6e2f5ff05e7460670
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-07-08 14:26:51 -0700

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if != a_dictionary:
+        return None
+    else:
+        best = ""
+        bestno = 0
+        for i, j in a_dictionary.items():
+            if j >= bestno:
+                bestno = j
+                best = i
+        return best
