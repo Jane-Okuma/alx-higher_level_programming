83b14ba3596eefe34080338f9dda09020bef91c2
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-07-20 06:24:49 -0700

    Comm2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a module
+"""
+
+
+class Square:
+    """Class square that defines a square by size
+    """
+    def __init__(self, size):
+        """Inializes the data
+        """
+        self.size = size
