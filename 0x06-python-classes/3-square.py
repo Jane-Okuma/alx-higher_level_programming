ee20a8f857e236c3209d49ccf111172a6fd709ac
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-07-20 07:32:40 -0700

    4

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A module
+"""
+
+
+class Square:
+    """Class square that defines a square by size and contains exceptions
+    """
+    def __init__(self, size=0):
+        """Initialize data and raise exceptions
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method that returns area of square
+        """
+        a = self.__size
+        return(a * a)
